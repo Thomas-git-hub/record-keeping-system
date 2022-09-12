695b4bc207656df7c94e7d0a96f193387bda6fdb
git-pixel-05 <77788923+git-pixel-05@users.noreply.github.com>
2022-09-12 17:37:08 +0800

    NEW UI

--- views/admin/includes/test.php
+++
@@ -1,5 +0,0 @@
-<h1>hello world</h1>
-    <p>dawdawdawdawdawdawdawdawdawdawd</p>
-    <div class="container" style="background-color: red; width: 1000px;">
-        <h1> HELLOW</h1>
-    </div>
---
+++ views/staff/includes/modal.php
@@ -0,0 +1,176 @@
+
+<!-- CREATE NEW ANNOUNCEMENT MODAL -->
+
+<div class="modal fade" id="createAnnouncement" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New Announcement</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
+        <form>
+
+            <div class="form-group">
+              <label for="exampleInputEmail1">Title</label>
+              <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Title of Event" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Date</label>
+              <input type="date" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Description</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+      </div>
+
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn modal-btn-baptismal">Create Now</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- CREATE NEW EVENTS MODAL -->
+
+<div class="modal fade" id="createEvent" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New Event</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
+        <form>
+
+            <div class="form-group">
+              <label for="exampleInputEmail1">Title</label>
+              <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Title of Announcement" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Date</label>
+              <input type="date" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Description</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+      </div>
+
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn modal-btn-baptismal">Create Now</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- CREATE BAPTISMAL MODAL -->
+
+<div class="modal fade" id="createBaptismal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Create New Baptismal</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
+        <form>
+
+            <div class="form-group">
+              <label for="exampleInputEmail1">Name</label>
+              <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter full name" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Fathers Name</label>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Mothers Name</label>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Birthdate</label>
+              <input type="date" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Year of Baptism</label>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Sponsors Name</label>
+              <input type="" class="form-control mb-2" id="exampleInputPassword1" placeholder="Sponsor 1" required>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="Sponsor 2" required>
+            </div>
+      </div>
+
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn modal-btn-baptismal">Create Now</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- CREATE MARRIAGE MODAL -->
+
+<!-- <div class="modal fade" id="createMarriage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Create New Marriage</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
+        <form>
+
+            <div class="form-group">
+              <label for="exampleInputEmail1">Name</label>
+              <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter full name" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Fathers Name</label>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Mothers Name</label>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Birthdate</label>
+              <input type="date" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Year of Baptism</label>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="" required>
+            </div>
+            <div class="form-group mt-2">
+              <label for="exampleInputPassword1">Sponsors Name</label>
+              <input type="" class="form-control mb-2" id="exampleInputPassword1" placeholder="Sponsor 1" required>
+              <input type="" class="form-control" id="exampleInputPassword1" placeholder="Sponsor 2" required>
+            </div>
+      </div>
+
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn modal-btn-baptismal">Create Now</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div> -->
